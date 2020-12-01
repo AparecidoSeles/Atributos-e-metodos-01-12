9c614a899014a5ddd44d6f144a8724e72bb13537
junior <you@example.com>
2020-12-01 10:26:40 -0300

    Atributos e Métodos concluido

---
+++ Personagem.cs
@@ -0,0 +1,19 @@
+namespace Atributos_e_metodos_01_12
+{
+    public class Personagem
+    {
+        public string nome;
+        public int idade;
+        public string armadura;
+
+        public string Atacar(){
+        return " Ataque de fogo! ";
+        
+        }
+
+        public string Defender(){
+            return " Parede de Água!";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Atributos_e_metodos_01_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem person1 =  new Personagem();
+            person1.nome = "Junior";
+            person1.idade = 0;
+            person1.armadura = "voadora";
+
+            Personagem person2 = new Personagem();
+            person2.nome = "joão";
+            person2.idade = 0;
+            person2.armadura = "gelo";
+
+            Console.WriteLine($"Personagem {person1.nome} usou um {person1.Atacar()}");
+
+            Console.WriteLine($"Perosonagem {person2.nome} se defendeu com uma {person2.Defender()}");
+         
+        }
+    }
+}
